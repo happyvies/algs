f0beafa338498a1ee1a17c7087f7208333cb63e7
Ruslan Karakotov <karrrakotov@mail.ru>
2023-07-20 17:38:53 +0300

    Added new solutions to 7-kyu

---
+++ codewars/7-kyu/pkg-2/main.go
@@ -0,0 +1,12 @@
+package kata
+
+// 7 kyu
+// https://www.codewars.com/kata/526c7363236867513f0005ca/train/go
+
+func IsLeapYear(year int) bool {
+	if year%400 == 0 || year%4 == 0 && year%100 != 0 {
+		return true
+	}
+
+	return false
+}
---
+++ codewars/7-kyu/pkg-3/main.go
@@ -0,0 +1,16 @@
+package kata
+
+// 7 kyu
+// https://www.codewars.com/kata/58941fec8afa3618c9000184/train/go
+
+func GrowingPlant(upSpeed, downSpeed, desiredHeight int) int {
+	var days, heightPlant int
+
+	for heightPlant < desiredHeight {
+		heightPlant += upSpeed
+		heightPlant -= downSpeed
+		days++
+	}
+
+	return days
+}
